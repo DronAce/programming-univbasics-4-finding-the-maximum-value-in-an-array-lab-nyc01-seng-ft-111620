7030849e62168f5fb27067c1701d8dcfb637ca70
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 12:41:22 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,5 +1,11 @@
 def find_max_value(array)
   # Add your solution here
   size = array.size
-  array.max
+  size.times do |index|
+    size.times do |n|
+      if array[index] > array[n]
+        return
+    end
+  end
+  # array.max
 end
